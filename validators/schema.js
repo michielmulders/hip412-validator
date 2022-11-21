242eeb4bc2478277e4181be59b022955febe2fa8
Michiel Mulders <michielmulders1@gmail.com>
2022-11-22 00:28:33 +0100

    Initial commit npm package

---
+++ validators/schema.js
@@ -0,0 +1,23 @@
+const Validator = require("jsonschema").Validator;
+const validator = new Validator();
+
+/*
+ * @desc: schema validator -> verifies instance against JSON schema HIP412@1.0.0
+ */
+const schemaValidator = (instance, schema) => {
+  const errors = [];
+  let result = validator.validate(instance, schema);
+
+  result.errors.forEach((error) => {
+    errors.push({
+      type: "schema",
+      msg: error.message,
+    });
+  });
+
+  return errors;
+};
+
+module.exports = {
+  schemaValidator,
+};
