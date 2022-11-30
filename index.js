52a5c86189fda51db74fa38e99f6595f2f66315f
Michiel Mulders <michielmulders1@gmail.com>
2022-11-30 10:33:47 +0100

    Separate warnings and errors

--- index.js
+++ index.js
@@ -2,6 +2,32 @@ const { schemaValidator, attributesValidator, localizationValidator, SHA256Valid
 const { getSchema, defaultVersion } = require("./schemas");
 
 /**
+ * Distil an errors array from JSON schema into errors and warnings. 
+ * We want to separate "additional property" errors into warnings because they don't influence the further validatin of the JSON object.
+ * 
+ * @param {Array} problems - Errors array from jsonschema
+ */
+const distilProblems = (problems) => {
+    const warnings = problems.find(problem => {
+        if (problem.msg.includes(additionalPropertyMsg)) return problem;
+        return null;
+    })
+    const filteredWarnings = warnings.filter(warning => !!warning)
+    console.log(filteredWarnings);
+    const errors = problems.find(problem => {
+        if (!problem.msg.includes(additionalPropertyMsg)) return problem;
+        return null;
+    })
+    const filteredErrors = errors.filter(error => !!error)
+    console.log(filteredErrors);
+
+    return {
+        warnings: filteredWarnings,
+        errors: filteredErrors
+    }
+}
+
+/**
  * Validate a metadata object against a schema. This function validates the instance respectively against
  * the schema validator, attributes validator, localization validator, and SHA256 validator. 
  * 
@@ -14,22 +40,25 @@ const { getSchema, defaultVersion } = require("./schemas");
  */
 const validator = (instance, schemaVersion = defaultVersion) => {
     let errors = [];
+    let warnings = [];
 
     const schema = getSchema(schemaVersion)
 
     // When errors against the schema are found, you don't want to continue verifying the NFT
-    const schemaErrors = schemaValidator(instance, schema);
-    if (schemaErrors.length > 0) {
+    const schemaProblems = schemaValidator(instance, schema);
+    if (schemaProblems.length > 0) {
         // However we don't want to continue if it only contains "additional property" errors because they don't hinder the further verification of the NFT
         const additionalPropertyMsg = "is not allowed to have the additional property";
-        const additionalPropertyCheck = schemaErrors.map(error => error.msg.includes(additionalPropertyMsg))
+        const additionalPropertyCheck = schemaProblems.map(problem => problem.msg.includes(additionalPropertyMsg))
 
         // If it contains at least one other type of error, we want to return the schema errors instead of continuing the verification process
         if (!additionalPropertyCheck.every(propertyCheck => propertyCheck === true)) {
-            return schemaErrors;
+            return distilProblems(schemaProblems);
         }
-9
-        errors.push(...schemaErrors);
+
+        const distilledProblems = distilProblems(schemaProblems);
+        errors.push(...distilledProblems.errors);
+        warnings.push(...distilledProblems.warnings);
     }
 
     const attributeErrors = attributesValidator(instance);
@@ -41,7 +70,10 @@ const validator = (instance, schemaVersion = defaultVersion) => {
     const SHA256Errors = SHA256Validator(instance);
     errors.push(...SHA256Errors);
 
-    return errors;
+    return {
+        errors,
+        warnings
+    };
 }
 
 module.exports = {
